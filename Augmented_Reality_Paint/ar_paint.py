e7436241ad11f7e8c0be39ff9643c7b64f2d957c
João Nunes <jpnunes06@ua.pt>
2021-11-17 21:14:44 +0000

    #4 It's now possible to paint with the mouse too

--- Augmented_Reality_Paint/ar_paint.py
+++ Augmented_Reality_Paint/ar_paint.py
@@ -43,6 +43,21 @@ set_default_style('light')
 # PSR, University of Aveiro, November 2021.
 # ---------------------------------------------------
 
+def onMouse(event, x, y, flags, param):
+
+    global isdown
+    global center_mouse
+
+    if event == cv2.EVENT_MOUSEMOVE:
+        if isdown:
+            center_mouse = (x, y)
+
+    if event == cv2.EVENT_LBUTTONDOWN:
+        isdown = True
+        center_mouse = (x, y)
+
+    if event == cv2.EVENT_LBUTTONUP:
+        isdown = False
 
 def main():
     # ---------------------------------------------------
@@ -72,13 +87,21 @@ def main():
 
     blank_image = 255 * np.ones(shape=[windowHeight, windowWidth, 3], dtype=np.uint8)
     cv2.imshow("White Blank", blank_image)
+    cv2.setMouseCallback("White Blank", onMouse)
 
+    global isdown
+    global center_mouse
+    isdown = False
     radio = 5
+    # global color
     color = (255, 0, 0)
     color_str = 'BLUE'
     center = (200, 200)
+    center_mouse = (200, 200)
     center_prev = (200, 200)
+    center_prev_mouse = (200, 200)
     print('You are painting with color ' + color_str + ' and pencil size ' + str(radio))
+
     if args['use_shake_prevention']:  # if the user uses the shake prevention
         print(Fore.BLUE + Back.WHITE + 'You are using shake prevention.' + Style.RESET_ALL)
         shake_prevention_on = 1
@@ -145,29 +168,45 @@ def main():
                 cv2.imwrite('drawing_' + date + '.png', blank_image)
                 print('Current image saved as: ' + Fore.BLUE + 'drawing_' + date + '.png' + Style.RESET_ALL)
 
-        if centroid is None:
+        if radio == 0:          # if the thickness of the line is zero the program doesn't draw
             pass
         else:
-            # Change the variable centroid to a tuple in center
-            center = (int(centroid[0]), int(centroid[1]))
-            if radio == 0:
-                pass
-            else:
+            if isdown:          # Code for when the user is pressing the mouse
                 if shake_prevention_on:  # if the user uses the shake prevention
-                    # Calculate the distance between the centroid detected and the previous centroid detected
-                    distance = math.sqrt(((center[0] - center_prev[0]) ** 2) + ((center[1] - center_prev[1]) ** 2))
+                    # Calculate the distance between the point of the mouse pressed and the previous point
+                    distance_mouse = math.sqrt(((center_mouse[0] - center_prev_mouse[0]) ** 2) + (
+                                (center_mouse[1] - center_prev_mouse[1]) ** 2))
+                    if distance_mouse > 40:
+                        center_prev_mouse = center_mouse  # defining the center_prev to use in the next cycle
+                    else:
+                        # Paint a line according to the inputs
+                        cv2.line(blank_image, center_prev_mouse, center_mouse, color, radio)
+                        center_prev_mouse = center_mouse  # defining the center_prev to use in the next cycle
+                else:
+                    # Paint a line according to the inputs
+                    cv2.line(blank_image, center_prev_mouse, center_mouse, color, radio)
+                    center_prev_mouse = center_mouse    # defining the center_prev to use in the next cycle
+            else:               # Code for when the user is not pressing the mouse
+                if centroid is None:
+                    pass
+                else:
+                    # Change the variable centroid to a tuple in center
+                    center = (int(centroid[0]), int(centroid[1]))
 
-                    if distance > 40:  # if the distance is bigger than a defined number, the program doesn't paint
-                        pass
+                    if shake_prevention_on:  # if the user uses the shake prevention
+
+                        # Calculate the distance between the centroid detected and the previous centroid detected
+                        distance = math.sqrt(((center[0] - center_prev[0]) ** 2) + ((center[1] - center_prev[1]) ** 2))
+                        if distance > 40:  # if the distance is bigger than a defined number, the program doesn't paint
+                            center_prev = center  # defining the center_prev to use in the next cycle
+                        else:
+                            # Paint a line according to the inputs
+                            cv2.line(blank_image, center_prev, center, color, radio)
+                            center_prev = center  # defining the center_prev to use in the next cycle
                     else:
                         # Paint a line according to the inputs
                         cv2.line(blank_image, center_prev, center, color, radio)
-
-                else:  # if the user doesn't use the shake prevention
-                    # Paint a line according to the inputs
-                    cv2.line(blank_image, center_prev, center, color, radio)
-
-                center_prev = center  # defining the center_prev to use in the next cycle
+                        center_prev = center  # defining the center_prev to use in the next cycle
 
         # Show the webcam frame, the mask and the image being painted
         cv2.imshow("Original", frame)
